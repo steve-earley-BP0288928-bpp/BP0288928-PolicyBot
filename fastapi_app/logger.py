95e94e14fe459c416023dd7d702a2610a6598b9a
steve-earley-BP0288928-bpp <147522039+steve-earley-BP0288928-bpp@users.noreply.github.com>
2025-07-19 19:24:17 +0100

    Update logger.py
    Adds datetime to log file to make it easier to prep the files for data analysis

--- fastapi_app/logger.py
+++ fastapi_app/logger.py
@@ -1,42 +1,53 @@
 import logging
-import sys 
+import sys
 import os
 from dotenv import load_dotenv
 from logtail import LogtailHandler
 from .globals import global_storage
 
-#load environment variables
+from datetime import datetime
+
+# load environment variables
 load_dotenv()
+
+# get the current date for the log filename
+now = datetime.now().strftime('%Y-%m-%d_%H-%M')
 
 token = os.getenv('LOGTAIL_TOKEN')
 
 # get logger
 logger = logging.getLogger("ragapp")
 
-# create formatter 
+# create formatter
+
 
 class CustomFormatter(logging.Formatter):
     def format(self, record):
-        record.model = getattr(global_storage, 'chat_model', 'No Model Selected')
+        record.model = getattr(
+            global_storage, 'chat_model', 'No Model Selected')
         record.summariser = getattr(global_storage, 'to_summarise', False)
         record.user_context = getattr(global_storage, 'user_context', {})
         record.chat_class = getattr(global_storage, 'chat_class', None)
-        record.message_history = " | ".join(global_storage.message_history)  # Join messages into a single string
+        # Join messages into a single string
+        record.message_history = " | ".join(global_storage.message_history)
         format_string = "%(asctime)s - %(levelname)s - Model: %(model)s - Summariser: %(summariser)s - Messages: %(message_history)s - User context: %(user_context)s - Chat Class: %(chat_class)s"
         formatter = logging.Formatter(format_string)
         return formatter.format(record)
 
+
 def handle_new_message(message):
     global_storage.message_history.append(message)
-    
-    if len(global_storage.message_history) > 6: 
+
+    if len(global_storage.message_history) > 6:
         global_storage.message_history = global_storage.message_history[-6:]
 
 
-
-formatter = CustomFormatter("%(asctime)s - %(levelname)s - Model: %(model)s - Summariser: %(summariser)s - %(message)s - User context: %(user_context)s - Chat Class: %(chat_class)s")
+formatter = CustomFormatter(
+    "%(asctime)s - %(levelname)s - Model: %(model)s - Summariser: %(summariser)s - %(message)s - User context: %(user_context)s - Chat Class: %(chat_class)s")
 
 # create ExcludeWarningsFilter class to remove unneccessary logs (e.g. "defaulting to Cl100k")
+
+
 class ExcludeWarningsAndHTTPFilter(logging.Filter):
     def filter(self, record):
         # Allow INFO, ERROR, and CRITICAL, but not WARNING
@@ -44,7 +55,7 @@ class ExcludeWarningsAndHTTPFilter(logging.Filter):
             return False
         if record.getMessage().startswith("HTTP Request"):
             return False
-    
+
         sensitive_keywords = [
             "Authenticating to PostgreSQL",
             "Creating Ollama Chat Client",
@@ -65,12 +76,13 @@ class ExcludeWarningsAndHTTPFilter(logging.Filter):
             return False
 
         # If none of the above conditions are met, allow the log
-        return True    
-# create handlers 
+        return True
+# create handlers
+
 
 # stream_handler = logging.StreamHandler(sys.stdout)
-file_handler = logging.FileHandler('app.log')
-better_stack_handler = LogtailHandler(source_token = token)
+file_handler = logging.FileHandler(f'app_log_{now}.log')
+better_stack_handler = LogtailHandler(source_token=token)
 
 # set formatters
 # stream_handler.setFormatter(formatter)
